fcc9451fa960451dc6ab22712833607c83b2777d
Kyle Gwinnup <kylegwinnup@gmail.com>
2024-04-16 07:01:10 -0400

    bump version

--- tests/unicode_util.test.ts
+++ tests/unicode_util.test.ts
@@ -1,8 +1,9 @@
-import { UnicodeCategory, Unicode } from "../mod";
+import { UnicodeCategory, Unicode, Char } from "../mod";
 
 describe("sum module", () => {
     test("checks Lu group", () => {
         const uc = new Unicode([UnicodeCategory.unicodeLu]);
         expect(uc.lookup("A".charCodeAt(0))).toBe(true);
+        expect(uc.lookup(Char.A)).toBe(true);
     });
 });
